496a51e77f319cf660fdffe22fccee11238b4b4d
harismumtaz070 <135727129+harismumtaz070@users.noreply.github.com>
2023-10-05 10:35:45 +0500

    change

--- index.js
+++ index.js
@@ -4,23 +4,29 @@ document
 
 function changeBodyColor(){
     let body = document.getElementsByTagName('body')
-    // console.log(body[0])
-    body[0].style.backgroundColor ='#413a3a'
+    body[0].style.backgroundColor ='#393938'
+
     let changecolor = document.getElementById('changeclrcontainer')
-    changecolor.style.backgroundColor = 'rgb(94, 94, 94)'
+    changecolor.style.backgroundColor = 'rgba(72,72,71,255)'
     changecolor.style.color = '#fff'
+
     let aboutus = document.getElementById('aboutus')
     aboutus.style.color = '#fff'
+
     let btn = document.getElementById('changeTheme')
     btn.style.backgroundColor = '#fff'
     btn.style.color = 'black'
     btn.style.fontWeight = 'bold'
+
     let home = document.getElementById('home')
     home.style.color = '#fff'
+
     let about = document.getElementById('about')
     about.style.color = '#fff'
+
     let service = document.getElementById('service')
     service.style.color = '#fff'
+
     let contact = document.getElementById('contact')
     contact.style.color = '#fff'
 }
